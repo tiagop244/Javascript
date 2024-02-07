195c599355d210dd3cfd350f9f8211425504edbf
tiago <tiago.duarte@portalsesisp.org.br>
2024-02-07 11:26:36 -0300

    javascript-basico

---
+++ 01-variaveis.js
@@ -0,0 +1,9 @@
+//Criando variaveis com "var" (var nome variavel = <valor;)
+
+var nome = "Tiago";
+var idade = 15;
+console.log("Meu nome é", nome, "e tenho", idade, "anos");
+
+console.log("Agora fiz aniversário!🎉 Tenho", idade + 1, "anos!");
+
+//console.log("agora fiz aniversario tenho", idade+1)
---
+++ 02-aritimetico.js
@@ -0,0 +1,21 @@
+//utilizando operadores aritimeticos com JavaScript
+
+console.log("A soma entre 8 e 7 é:", 8 + 7);
+
+//efetuar a soma de dois numeros informados por voce (var nome variavel =12)
+var x = 15;
+var y = 5;
+console.log("A soma das variaveis", x, "e", y, "é:", x + y);
+
+var soma = x + y;
+var subtracao = x - y;
+var multiplicacao = x * y;
+var divisao = x / y;
+
+console.log("soma = ", soma);
+console.log("subtracao = ", subtracao);
+console.log("multiplicacao = ", multiplicacao);
+console.log("divisao = ", divisao);
+
+//regras para nomear identificadores (var, const ou let)
+//podem comecar com A-Z, a-z, _ou $
