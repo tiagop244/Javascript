04f0072f91dc28f4aaa1b49e501ae2ae059f6da7
tiago <tiago.duarte@portalsesisp.org.br>
2024-02-20 17:01:42 -0300

    vetor dinossauros

---
+++ 12-Vetor.js
@@ -0,0 +1,24 @@
+var dinos = ["Tiranossauro Rex", "Estegossauro", "Anquilossauro"];
+console.log(dinos);
+console.log(dinos[0]);
+console.log("");
+console.table(dinos);
+console.log("O vetor tem", dinos.length, "elementos!");
+
+dinos.push("Brontossauro");
+console.table(dinos);
+dinos.push("Velociraptor");
+console.table(dinos);
+dinos.unshift("Triceratops");
+console.table(dinos);
+dinos[3] = "Titanosauro";
+console.table(dinos);
+dinos.pop();
+console.table(dinos);
+dinos.pop();
+console.table(dinos);
+4;
+dinos.pop();
+console.table(dinos);
+dinos.pop();
+console.table(dinos);
